597aa821c68b9965a3a166684b59c3b721c88daa
donatimar <donatimar@gmail.com>
2024-10-21 15:15:01 +0200

    04 - Saluti

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,13 +1,14 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const name = 'Mario';
-
+const name = "Mario";
 
 // Dichiara la funzione qui.
-
+function greet(name) {
+  return `Ciao ${name}`;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const greetings = greet(name);
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
+console.log(greetings);
