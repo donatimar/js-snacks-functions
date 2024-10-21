44d7fdeb7295a1d49b50d28aaf64c7251ed5da3d
donatimar <donatimar@gmail.com>
2024-10-21 14:41:41 +0200

    01 - iniziali

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -2,12 +2,19 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
-
+function nameInitials(array) {
+  const initials = []; //
+  for (let i = 0; i < array.length; i++) {
+    console.log("Nome:", array[i]);
+    console.log("Iniziale:", array[i][0]);
+    initials.push(array[i][0]);
+  }
+  return initials;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const result = nameInitials(names);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+console.log("Risultato finale:", result); // Stampa il risultato finale in console
