f65d4664e180cc8066869cf0477bb9f355ba1b0b
donatimar <donatimar@gmail.com>
2024-10-21 14:55:04 +0200

    02 - Filtra iniziali

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -2,12 +2,20 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
-
 // Dichiara la funzione qui.
+function filterLetter(array, letter) {
+  console.log("Array:", array);
+  console.log("Lettera:", letter);
 
+  return array.filter(function (word) {
+    const startLetter = word.indexOf(letter) === 0;
+    console.log(`Verifico se ${word} inizia con ${letter}:`, startLetter);
+    return startLetter;
+  });
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const result = filterLetter(names, "A");
+console.log("Risultato:", result);
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
